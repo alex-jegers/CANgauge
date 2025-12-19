85b561ed57a2f5fec19f7aae792dbf9846757ecc
Alex Jegers <jegers.alex@gmail.com>
2025-12-19 14:53:37 -0600

    Initial ADC driver and battery monitor commit

---
+++ cm7/application/app_battery_monitor.c
@@ -0,0 +1,15 @@
+/**********     INCLUDES        **********/
+#include "app_battery_monitor.h"
+#include "common/drivers/stm32_adc.h"
+
+/**********		DEFINES		**********/
+
+/**********		EXTERNAL VARIABLE DEFINITIONS		**********/
+
+/**********		STATIC VARIABLES		**********/
+
+/**********		STATIC FUNCTION DECLRATIONS		**********/
+
+/**********		STATIC FUNCTION DEFINITIONS		**********/
+
+/**********		GLOBAL FUNCTION DEFINITIONS		**********/
---
+++ cm7/application/app_battery_monitor.h
@@ -0,0 +1,24 @@
+
+#ifndef _TEMPLATE_H_
+#define _TEMPLATE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**********     INCLUDES        **********/
+
+/**********     DEFINES      **********/
+
+/**********     GLOBAL VARIABLE DECLRATIONS     **********/
+
+/**********		GLOBAL FUNCTION DECLRATIONS		**********/
+
+
+
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif  //_TEMPLATE_H_
