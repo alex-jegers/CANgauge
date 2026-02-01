abc2936caead97bf8da259f190ad62862f793186
Alex Jegers <jegers.alex@gmail.com>
2026-02-01 15:54:41 -0600

    Squashed commit of the following:
    commit 7c8e3f8adce48d044508cc7a5e2dc9f392177d3b
    Author: Alex Jegers <jegers.alex@gmail.com>
    Date:   Sun Feb 1 15:54:31 2026 -0600
    
        Abstraction layer for writing to flash
    
    commit 3cfbb32a824aed5e75bcc97044282c1c10b3c145
    Author: Alex Jegers <jegers.alex@gmail.com>
    Date:   Sun Feb 1 15:48:15 2026 -0600
    
        Commit

---
+++ cm7/system/system_mem.h
@@ -0,0 +1,35 @@
+
+#ifndef _SYSTEM_MEM_H_
+#define _SYSTEM_MEM_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**********     INCLUDES        **********/
+#include "stm32h745xx.h"
+
+/**********     DEFINES      **********/
+#define SYSTEM_MEM_REGION_EXTERN_RAM			__attribute__((__section__(".ext_mem_ram")))
+#define SYSTEM_MEM_REGION_NVM                   __attribute__((__section__(".config")))
+
+#define SYSTEM_MEM_CONFIG_LENGTH_BYTES			0x100
+
+/**********     GLOBAL VARIABLE DECLRATIONS     **********/
+typedef uint32_t flash_uint32_t;
+typedef uint8_t flash_uint8_t;
+typedef void* flash_void_t;
+
+/**********		GLOBAL FUNCTION DECLRATIONS		**********/
+void system_mem_save_nvm_u32(flash_uint32_t* p, uint32_t val);
+void system_mem_save_nvm_u8(flash_uint8_t* p, uint8_t val);
+void system_mem_save_nvm_void(flash_void_t* p, void* src, uint32_t size);
+
+
+
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif  //_SYSTEM_MEM_H_
