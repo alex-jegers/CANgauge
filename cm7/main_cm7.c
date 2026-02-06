93c84cfdd4ad30065d673073b8edb36e8461c4af
Alex Jegers <jegers.alex@gmail.com>
2026-02-05 21:45:00 -0600

    Update main_cm7.c

--- cm7/main_cm7.c
+++ cm7/main_cm7.c
@@ -44,7 +44,9 @@ int main(void)
 	touch_scr_run(p_touch_data);						//Runs the touch screen task.
 		
 	/* Load the menu screen. */
-	app_menu_run();
+	//app_menu_run();
+	lv_demo_benchmark();
+
 
 	/* Starts the FreeRTOS scheduler. */
 	vTaskStartScheduler();
