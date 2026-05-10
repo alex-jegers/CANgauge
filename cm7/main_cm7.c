67a420bc1e54c8cbf53599076c932d755f7d4959
Alex Jegers <awjpp@outlook.com>
2026-05-10 11:15:15 -0500

    Adding new USB tasks

--- cm7/main_cm7.c
+++ cm7/main_cm7.c
@@ -39,6 +39,9 @@ int main(void)
 
 	pwr_monitor_run(4);
 
+	usb_msc_task_run();
+
+	usb_watchdog_run();
 	/* Creates a task to finish the rest of the system initialization. */
    	xTaskCreate(system_task_init, "SYS_INIT", 450, NULL, 5, NULL);
 
