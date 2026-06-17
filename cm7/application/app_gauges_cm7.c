be47d4d07e82747a49c2697903ad76acaac808be
Alex Jegers <awjpp@outlook.com>
2026-06-17 11:39:45 -0500

    Gauges app changes
    Lots of small changes and refactoring.

--- cm7/application/app_gauges_cm7.c
+++ cm7/application/app_gauges_cm7.c
@@ -1,7 +1,6 @@
 /**********     INCLUDES        **********/
+#include <application/can_uds_def.h>
 #include "app_gauges_cm7.h"
-#include "app_gauges_prv.h"
-
 #include "applications_cm7.h"
 
 #include "ui/ui_gauges.h"
@@ -23,19 +22,15 @@
 /**********		EXTERNAL VARIABLE DEFINITIONS		**********/
 
 /**********		STATIC VARIABLES		**********/
-const char* prv_version = "v0.4.3";
+const char* prv_version = "v0.4.4";
 static bool prv_task_run = false;
 static TaskHandle_t prv_gauges_task_handle;
 static EventGroupHandle_t prv_event_group = NULL;
 static saej1979_current_data_t* active_param[4] = { NULL, NULL, NULL, NULL };
-static can_transmit_handle_t* prv_current_data_query[4] = { NULL, NULL, NULL, NULL };
-static uint32_t prv_can_id = 0x00;
-static can_id_t prv_id_type = 0x00;
 
 /**********		STATIC FUNCTION DECLRATIONS		**********/
 static void prv_task_gauges();							//The FreeRTOS task.
 static void prv_load_gauges(const char *str[4], uint8_t num_gauges);
-static bool prv_update_available_uds_data();			//Checks to see if the CAN controller task found UDS data, returns false if there's nothing there.
 static void prv_create_gauge_select_btns();				//Creates the buttons on the GUI.
 
 static void prv_gauge_event_cb(lv_event_t* e);			//Handler for the gauge itself events.
@@ -55,14 +50,6 @@ static void prv_task_gauges()
 {
 	/* Load the UI. */
 	ui_gauges_load();
-
-	//TODO: This check for demo mode is obsolete bc were starting from gauges now not the main menu.
-	if (ui_helpers_is_demo_mode())
-	{
-		/* Stop the task but dont block at all because we're calling from in the task just to set prv_run to false. */
-		app_gauges_stop(pdMS_TO_TICKS(0));
-		vTaskDelete(NULL);
-	}
 
 	/* Turn on the CAN peripheral and set the baud rate. */
 	can_init(FDCAN1);
@@ -98,9 +85,6 @@ static void prv_task_gauges()
 
 	if (found_car == true)
 	{
-		prv_can_id = app_can_controller_get_can_id();
-		prv_id_type = (prv_can_id > 0x7ff) ? CAN_ID_XTD : CAN_ID_STD;
-		prv_update_available_uds_data();		//Gets data from CAN controller and sets the "available" variable in the data array.
 		prv_create_gauge_select_btns();			//Creates the buttons in the UI.
 	}
 	else
@@ -219,10 +203,10 @@ static void prv_task_gauges()
 				float offset = active_param[d]->offset;
 				float processed_val = ((float)raw_value * scale) + offset;
 
-				if (processed_val == last_value[d])
-				{
-					continue;
-				}
+				//if (processed_val == last_value[d])
+				//{
+			//		continue;
+				//}
 
 				if (lv_port_take_lvgl_mutex(portMAX_DELAY))
 				{
@@ -249,43 +233,39 @@ static void prv_task_gauges()
 
 }
 
-static bool prv_update_available_uds_data()
-{
-	uint8_t num_params = 0;
-
-	/* Check the generic parameters. */
-	for (uint8_t x = 0; x < 0x80; x += 0x20)
-	{
-		uint32_t available_pids_1 = can_controller_get_data(x, 0, 4);
-		for (int8_t i = 31; i >= 0; i--)
-		{
-			saej1979_current_data_t* y = saej1979_get_current_data(32 - i + x);
-			if ((available_pids_1 & (1 << i)) != 0)
-			{
-				y->available = true;
-				num_params++;
-			}
-			else
-			{
-				y->available = false;
-			}
-		}
-	}
-	
-
-	return num_params;
-}
-
 static void prv_create_gauge_select_btns()
 {
 	for (uint8_t i = 0; i < 176; i++)
 	{
 		saej1979_current_data_t* y = saej1979_get_current_data(i);
-		if ((y->available == true) && (y->min != y->max))
+		if ((y->available == true) && ((y->min != y->max) || (y->nested != NULL)))
 		{
-			lv_port_take_lvgl_mutex(portMAX_DELAY);
-			ui_gauges_create_gauge_checkbox(saej1979_get_current_data(i)->name);
-			lv_port_give_lvgl_mutex();
+			const char* txt;
+			/* Check if this one is nested. */
+			if (y->nested != NULL)
+			{
+				/* If it is, cycle through its parameters. */
+				for (uint8_t idx = 0; idx < 8; idx++)
+				{
+					saej1979_current_data_t* x = y->nested[idx];
+					if (x == NULL) { continue; }
+					if (x->available)
+					{
+						txt = x->name;
+						lv_port_take_lvgl_mutex(portMAX_DELAY);
+						ui_gauges_create_gauge_checkbox(txt);
+						lv_port_give_lvgl_mutex();
+					}
+				}
+
+			}
+			else
+			{
+				txt = y->name;
+				lv_port_take_lvgl_mutex(portMAX_DELAY);
+				ui_gauges_create_gauge_checkbox(txt);
+				lv_port_give_lvgl_mutex();
+			}
 		}
 	}
 }
@@ -293,10 +273,7 @@ static void prv_create_gauge_select_btns()
 static void prv_gauge_event_cb(lv_event_t* e)
 {
 	/* Stop transmitting the requestor on CAN. */
-	can_transmit_set_inactive(prv_current_data_query[0]);
-	can_transmit_set_inactive(prv_current_data_query[1]);
-	can_transmit_set_inactive(prv_current_data_query[2]);
-	can_transmit_set_inactive(prv_current_data_query[3]);
+	can_uds_stop_query();
 	xEventGroupClearBits(prv_event_group, EVENT_BITS_QUERY_TRANSMITTING);
 
 	/* Write to the save state file. */
@@ -326,10 +303,28 @@ static void prv_load_gauges(const char* str[4], uint8_t num_gauges)
 		const char *txt = str[g];
 		for (uint8_t i = 0; i < 176; i++) {
 			saej1979_current_data_t *x = saej1979_get_current_data(i);
-			/* Check to see if this PID is supported by CANgauge, continue if not. */
+
+			/* Check to see if this PID is supported. */
 			if (x->available == false) {
 				continue;
 			}
+
+			/* Check if it's nested. */
+			if (x->nested != NULL)
+			{
+				/* Cycle through the nested parameters. */
+				for (uint8_t idx = 0; idx < 8; idx++)
+				{
+					saej1979_current_data_t* y = x->nested[idx];
+					if (y == NULL) { continue; }
+					if (strcmp(y->name, txt) == 0)
+					{
+						x = y;
+						break;
+					}
+				}
+			}
+
 			/* Check to see if the checkbox text matches the PID text. */
 			if (strcmp(x->name, txt) == 0) {
 				/* For gauges on the right side of the screen we want to swap the min and max values. */
@@ -348,7 +343,7 @@ static void prv_load_gauges(const char* str[4], uint8_t num_gauges)
 	uint8_t pid1 = (active_param[1] == NULL) ? 0 : active_param[1]->pid_code;
 	uint8_t pid2 = (active_param[2] == NULL) ? 0 : active_param[2]->pid_code;
 	uint8_t pid3 = (active_param[3] == NULL) ? 0 : active_param[3]->pid_code;
-	saej1979_set_current_data_query(pid0, pid1, pid2, pid3);
+	can_uds_set_current_data_query(pid0, pid1, pid2, pid3);
 	xEventGroupSetBits(prv_event_group, EVENT_BITS_QUERY_TRANSMITTING);
 	ui_load_gauge_screen();
 }
@@ -479,93 +474,6 @@ bool app_gauges_stop(uint32_t block_time_ms)
     {
         return pdFALSE;
     }
-}
-
-
-void saej1979_set_current_data_query(uint8_t pid1, uint8_t pid2, uint8_t pid3, uint8_t pid4)
-{
-	/* Data byte 2 needs to be changed depending on the data being requested. */
-	can_tx_buffer_entry_t iso15765_query =
-	{
-		.T0.bit.ID = prv_can_id, .T0.bit.XTD = prv_id_type, .T0.bit.RTR = CAN_RTR_DATA_FRAME,
-		.T1.bit.DLC = 8, .T1.bit.EFC = 0, .T1.bit.BRS = 0, .T1.bit.FDF = 0,
-		.data[0] = 0x02, .data[1] = 0x01, .data[2] = 0x00, .data[3] = 0xCC,
-		.data[4] = 0xCC, .data[5] = 0xCC, .data[6] = 0xCC, .data[7] = 0xCC,
-	};
-
-	/* Create a CAN TX message if it hasnt been created already. */
-	if (prv_current_data_query[0] == NULL)
-	{
-		prv_current_data_query[0] = can_transmit_create_msg();
-		prv_current_data_query[1] = can_transmit_create_msg();
-		prv_current_data_query[2] = can_transmit_create_msg();
-		prv_current_data_query[3] = can_transmit_create_msg();
-	}
-
-	/* Determine how many PIDs are being passed in. */
-	int8_t num_pids = 4;
-	if (pid1 == 0) { return; }
-	else if (pid2 == 0) { num_pids = 1; }
-	else if (pid3 == 0) { num_pids = 2; }
-	else if (pid4 == 0) { num_pids = 3; }
-	//iso15765_query.data[0] = num_pids + 1;		//This sets the data length code (DLC).
-	/* These set the data field. */
-	iso15765_query.data[2] = pid1;
-
-	for (;num_pids > 0; num_pids--)
-	{
-		switch (num_pids - 1)
-		{
-		case 0:
-			iso15765_query.data[2] = pid1;
-			break;
-		case 1:
-			iso15765_query.data[2] = pid2;
-			break;
-		case 2:
-			iso15765_query.data[2] = pid3;
-			break;
-		case 3:
-			iso15765_query.data[2] = pid4;
-			break;
-		}
-		/* Add the CAN message, set its transmit period, and activate it. */
-		can_transmit_set_msg_data(prv_current_data_query[num_pids - 1], &iso15765_query);
-		can_transmit_set_period(prv_current_data_query[num_pids - 1], 25);
-		can_transmit_set_active(prv_current_data_query[num_pids - 1]);
-	}
-
-	return;
-}
-
-int32_t saej1979_current_data_process_data(can_rx_buffer_entry_t* input)
-{
-	uint32_t id = input->R0.bit.ID;
-	uint8_t pid = input->data[2];
-	uint32_t data = 0;
-	uint32_t num_params = sizeof(saej1979_current_data_arr) / sizeof(saej1979_current_data_t*);
-    for (uint8_t i = 0; i < num_params; i++)
-	{
-		if (pid == saej1979_current_data_arr[i]->pid_code)
-		{
-			if (saej1979_current_data_arr[i]->data_bytes == 1)
-			{
-				data = input->data[3];
-			}
-			else if (saej1979_current_data_arr[i]->data_bytes == 2)
-			{
-				data = (input->data[3] << 8) | input->data[4];
-			}
-			//data = saej1979_current_data_arr[i]->conversion_func(data);
-			return data;
-		}
-	}	
-	return 0;
-}
-
-saej1979_current_data_t* saej1979_get_current_data(uint8_t pid)
-{
-	return saej1979_current_data_arr[pid];
 }
 
 static void prv_brightness_slider_handler(lv_event_t* e)
