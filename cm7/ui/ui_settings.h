8f67daf304af7f00b0ae904ea71bdd8259fe6c33
Alex Jegers <awjpp@outlook.com>
2026-06-19 20:07:27 -0500

    Added units dropdowns

--- cm7/ui/ui_settings.h
+++ cm7/ui/ui_settings.h
@@ -26,13 +26,16 @@ extern "C" {
  */
 void ui_settings_load();
 void ui_settings_init();
+void ui_settings_delete();
 void ui_set_brightness_slider_event_cb(lv_event_cb_t func);				//The brightness slider is interacted with.
 void ui_set_settings_scr_load_event_cb(lv_event_cb_t func);				//The settings screen loads.
 void ui_set_settings_back_btn_event_cb(lv_event_cb_t func);				//The back button within the settings screen is pressed.
 void ui_add_settings_firmware_update_btn_event_cb(lv_event_cb_t func);	//The button to start a firmware update.
 void ui_set_settings_data_trnsf_btn_event_cb(lv_event_cb_t func);		//Used to connect the EEPROM file system over USB.
-bool ui_settings_set_pressure_units_dropdown(const char* units_str);	//Units_str must be PSI, kPa, or bar. Anything else will return false.
-bool ui_settings_set_temperature_units_dropdown(const char* units_str);	//Units_str must be C or F. Anything else will return false.
+bool ui_settings_set_pressure_units_dropdown(char* units_str);	//Units_str must be PSI, kPa, or bar. Anything else will return false.
+bool ui_settings_set_temperature_units_dropdown(char* units_str);	//Units_str must be C or F. Anything else will return false.
+void ui_settings_get_pressure_units_dropdown(char* buf);
+void ui_settings_get_temperature_units_dropdown(char* buf);
 
 #ifdef __cplusplus
 } /*extern "C"*/
