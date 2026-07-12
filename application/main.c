f0f79c1e96bbd4e06adc6b34baa8ab4cfe806656
Alex Jegers <awjpp@outlook.com>
2026-07-12 18:31:22 -0500

    Moving template files

---
+++ application/main.c
@@ -0,0 +1,39 @@
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @author         : A. Jegers
+ * @brief          : Main program body
+ ******************************************************************************
+ *
+ ******************************************************************************
+ */
+#include "cangauge.h"
+#include "application/applications_cm7.h"
+
+int main(void)
+{
+	/* Configures the clocks to run at full speed in full power mode. */
+ 	rcc_main_clock_config();
+
+	/* Initializes the external SDRAM. */
+	fmc_init_sdram();
+
+	/* Enable the hardware floating point unit (ensure this is enabled in project settings too). */
+	system_init_fpu();
+
+	system_init();
+
+	/* Creates a task to finish the rest of the system initialization. */
+   	xTaskCreate(system_task_init, "SYS_INIT", 650, NULL, 5, NULL);
+
+	/* Load the menu screen. */
+	system_set_ui_init_cb(app_gauges_run);
+
+	/* Starts the FreeRTOS scheduler. */
+	vTaskStartScheduler();
+
+	/* Should never reach here. */
+   	while(1);
+}
+
+
