b90f31186fd3a356b7304247242180244e23c4ba
Alex Jegers <awjpp@outlook.com>
2026-08-25 19:52:14 -0500

    Initial commit for graph ui

---
+++ application/ui/ui_graph.h
@@ -0,0 +1,32 @@
+
+#ifndef _UI_GRAPH_H_
+#define _UI_GRAPH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**********     INCLUDES        **********/
+
+/**********     TYPEDEFS         **********/
+
+/**********     DEFINES      **********/
+
+/**********     GLOBAL VARIABLE DECLRATIONS     **********/
+
+/**********		GLOBAL FUNCTION DECLRATIONS		**********/
+/**
+ * name:
+ *      desc:
+ *      params:
+ *      returns:
+ */
+void ui_graph_init();
+
+
+
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
+#endif  //_TEMPLATE_H_
