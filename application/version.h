f03a6cb79091ec3d4c7c51fd49893864c7452663
Alex Jegers <awjpp@outlook.com>
2026-09-24 15:31:58 -0500

    Update version.h

--- application/version.h
+++ application/version.h
@@ -19,7 +19,7 @@ extern "C" {
 	#define BUILD_TYPE_STR		"Dbg Ofast"
 #endif
 
-#define VERSION					"v1.4.41 09/20/26 06:28PM"
+#define VERSION					"v1.4.43 09/20/26 08:47PM"
 /**********     GLOBAL VARIABLE DECLRATIONS     **********/
 
 /**********		GLOBAL FUNCTION DECLRATIONS		**********/
